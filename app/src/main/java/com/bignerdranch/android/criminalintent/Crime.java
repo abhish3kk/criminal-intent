727747d968f2588b7049085a1055eb0b03d5abdf
Abhishek <abhishekgitlab@gmail.com>
2016-08-13 16:51:21 +0530

    Initial commit. Completed chapter 9. Page 192/214/640

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,53 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by abhishek on 8/9/16.
+ */
+
+public class Crime {
+
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDate() {
+        SimpleDateFormat df2 = new SimpleDateFormat("EEE, dd MMM yyyy");
+        return df2.format(mDate);
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
